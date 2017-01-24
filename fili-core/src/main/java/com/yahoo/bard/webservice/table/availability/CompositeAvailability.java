7a7fe7a0d5968fc8c9072f56163b4f3fac73bb58
Michael McLawhorn <mclawhor@yahoo-inc.com>
2017-01-24 17:57:55 -0600

    First draft availability compositing

--- fili-core/src/main/java/com/yahoo/bard/webservice/table/availability/CompositeAvailability.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/table/availability/CompositeAvailability.java
@@ -2,28 +2,18 @@
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 package com.yahoo.bard.webservice.table.availability;
 
-import com.yahoo.bard.webservice.data.dimension.Dimension;
 import com.yahoo.bard.webservice.data.dimension.DimensionColumn;
-import com.yahoo.bard.webservice.data.dimension.DimensionDictionary;
-import com.yahoo.bard.webservice.data.metric.MetricColumn;
 import com.yahoo.bard.webservice.table.Column;
 import com.yahoo.bard.webservice.table.PhysicalTable;
 import com.yahoo.bard.webservice.util.SimplifiedIntervalList;
 
 import org.joda.time.Interval;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * An availability which guarantees immutability on its contents.
@@ -32,8 +22,7 @@ public class CompositeAvailability implements Availability {
 
     private final Set<Availability> dependantAvailabilities;
 
-    private Function<List<List<Interval>>, List<Interval>>
-    //private final Map<Column, Supplier<List<Interval>>> foo;
+    private final Function<List<List<Interval>>, List<Interval>> metricIntervalReducer;
 
     /**
      * Constructor.
@@ -43,14 +32,17 @@ public class CompositeAvailability implements Availability {
     public CompositeAvailability(Set<PhysicalTable> physicalTables) {
         dependantAvailabilities = physicalTables.stream()
                 .map(PhysicalTable::getAvailability).collect(Collectors.toSet());
+
+        metricIntervalReducer = (metricAvailabilitiesList) -> {
+            if (metricAvailabilitiesList.size() > 1) {
+                throw new RuntimeException("Good exception message for invalid availability");
+            }
+            return metricAvailabilitiesList.size() == 0 ?
+                    new SimplifiedIntervalList() :
+                    metricAvailabilitiesList.get(0);
+        };
     }
 
-
-    Function<List<List<Interval>>, Stream<List<Interval>>>
-
-//List<Interval>
-
-    @Override
     public List<Interval> get(Column column) {
         if (column instanceof  DimensionColumn) {
             Optional<SimplifiedIntervalList> intervals = dependantAvailabilities.stream()
@@ -63,16 +55,21 @@ public class CompositeAvailability implements Availability {
                 .map(availability -> availability.get(column))
                 .collect(Collectors.toList());
 
-        return validator.apply(metricIntervalLists);
+        return metricIntervalReducer.apply(metricIntervalLists);
     }
 
     @Override
-    public Collection<List<Interval>> values() {
-        return availabilities.values();
+    public Set<List<Interval>> values() {
+        return keySet().stream()
+                .map(this::get)
+                .collect(Collectors.toSet());
     }
 
     @Override
     public Set<Column> keySet() {
-        return availabilities.keySet();
+        return dependantAvailabilities.stream()
+                .map(Availability::keySet)
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet());
     }
 }
